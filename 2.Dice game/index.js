68ba54ef5a601addfd9c2219b7cd34e7968a04e5
rangerbd <dipto.g1@gmail.com>
2025-04-08 18:37:01 +0200

    adding score

--- 2.Dice game/index.js
+++ 2.Dice game/index.js
@@ -1,3 +1,4 @@
+//adding random number and adding according to the image
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 var diceImage = "dice" + randomNumber1 + ".png"
 var randomImageSource = "images/" + diceImage
@@ -10,4 +11,13 @@ image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource)
 
 image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src",randomImageSource2)
+image2.setAttribute("src",randomImageSource2);
+
+
+
+if (randomNumber1 == randomNumber2)
+    document.querySelector("h1").innerHTML = "Its a Draw";
+else if (randomNumber1 > randomNumber2)
+    document.querySelector("h1").innerHTML = "Player 1 win";
+else
+    document.querySelector("h1").innerHTML = "Player 2 win";
